dfad72d412cd91ee9ce62110b308f012a12c0d77
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-11 15:26:44 -0500

    refactor tests for clarity

--- test/com/bjorn/BoardTest.java
+++ test/com/bjorn/BoardTest.java
@@ -1,5 +1,6 @@
 package com.bjorn;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -10,17 +11,22 @@ import static org.junit.Assert.assertEquals;
  * Created by bjornjohnson on 1/11/16.
  */
 public class BoardTest {
+    private Board newBoard;
+    private HashMap<String, String> boardState;
+
+    @Before
+    public void setUp() {
+        newBoard = new Board();
+        boardState = new HashMap<String, String>();
+    }
+
     @Test
     public void getNewBoardState() throws Exception {
-        Board newBoard = new Board();
-        HashMap<String, String> boardState = new HashMap<String, String>();
         assertEquals(boardState, newBoard.getBoardState());
     }
 
     @Test
     public void getBoardwithSingleShip() throws Exception {
-        Board newBoard = new Board();
-        HashMap<String, String> boardState = new HashMap<String, String>();
         boardState.put("a1", "shipOne" );
         boardState.put("a2", "shipOne" );
         newBoard.placeShip("a1", "right", 2);
